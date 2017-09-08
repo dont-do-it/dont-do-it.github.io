e30c86077e31e7b087adfd6bb7824df00f3cfff5
Sirfame <sirfame@hotmail.com>
2017-09-07 17:00:47 -0700

    timer almost working

--- js/app.js
+++ js/app.js
@@ -1,39 +1,60 @@
 'use-strict';
 
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-
-
 var serverTime = new Date();
-function updateTime() {
-    /// Increment serverTime by 1 second and update the html for '#time'
+
+function updateDeaths() {
+    var total2015Suicides = 788000;
+    var currentYearlyRate = 788000;
+    var currentRatePerMinute = currentYearlyRate / 365 / 24 / 60;
     serverTime = new Date(serverTime.getTime() + 1000);
-    $('#time').html(serverTime.toGMTString());
+    var newYears2015 = new Date("January 1, 2015, 00:00:00")
+    var deathsSince = Math.ceil((serverTime.getTime() - newYears2015.getTime()) / 1000 / 60 * currentRatePerMinute);
+    var totalDeaths = total2015Suicides + deathsSince;
+    $('#deaths').html(totalDeaths + " souls who have commited suicide,");
+    return totalDeaths
+}
+
+function updateFamily(totalDeaths) {
+    var familyMembersAffected = totalDeaths * 3;
+    $('#family').html(familyMembersAffected + " spouses, moms, dads, and siblings have lost their loved ones to suicide,");
+    return familyMembersAffected;
+}
+
+function updateFriends(totalDeaths) {
+    var friendsAffected = totalDeaths * 5;
+    $('#friends').html(friendsAffected + " people lost their friend to suicide,");
+    return friendsAffected;
+}
+
+function updateBrokenHearts(totalDeaths, familyTotal, friendsTotal) {
+    var brokenHearts = totalDeaths + familyTotal + friendsTotal;
+    $('#brokenHearts').html(brokenHearts + " broken hearts because of suicide.");
+    return brokenHearts
 }
 
 $(function() {
-    updateTime();
-    setInterval(updateTime, 1000);
-})
-
-var total2015 = 788000;
-var currentYearlyRate = 788000;
-var currentDailyRate = currentYearlyRate / 365;
-
-var newYears2015 = new Date('2015-01-01');
-var daysSince = Math.ceil((serverTime.getTime() - newYears2015.getTime()) / (1000 * 60 * 60 * 24));
-console.log(daysSince)
-
-var deathsNotInclusiveCurrentYear = total2015 * (today.getFullYear() - 2016)
-var deathsNotInclusiveCurrentMonth = total2015 * (today.getFullYear() - 2016)
-
-
-var totalDeaths = total2015 + deathsNotInclusiveCurrentMonth +
-
-console.log(deathsNotInclusiveCurrentYear)
+    var newTotal = updateDeaths();
+    var newFamilyTotal = updateFamily(newTotal);
+    var newFriendsTotal = updateFriends(newTotal);
+    var newBrokenHeartsTotal = updateBrokenHearts(newTotal, newFamilyTotal, newFriendsTotal);
+    setInterval(updateDeaths, 1000);
+    setInterval(updateFamily(updateDeaths()), 1000);
+    setInterval(updateFriends(updateDeaths()), 1000);
+    setInterval(updateBrokenHearts(newTotal, newFamilyTotal, newFriendsTotal), 1000);
+});
 
 var paragraph = "If you know someone who is thinking about suicide, or someone you think MIGHT be thinking of suicide, do yourself a favor and ask them how they are doing. Worst case scenario, you're asking a friend how their day is. Best case? You might just save a life."
+
 $('#paragraph').text(paragraph)
+
+
+
+// function updateTime() {
+//     serverTime = new Date(serverTime.getTime() + 1000);
+//     $('#time').html(serverTime.toGMTString());
+// }
+//
+// $(function() {
+//     updateTime();
+//     setInterval(updateTime, 1000);
+// })
