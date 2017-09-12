a02f0f7a33d4421bf41c8abd4aebf325cd4e1d98
Sirfame <sirfame@hotmail.com>
2017-09-12 14:13:11 -0700

    sectioning for UI work, and words

--- js/app.js
+++ js/app.js
@@ -79,7 +79,7 @@ $(function() {
     }, 750);
 });
 
-var paragraph1 = "If you are thinking of committing suicide, please reconsider. Listed below are resources available for you to find emotional refuge."
+var paragraph1 = "If you are thinking of committing suicide, please don't. Listed below are resources available for you to find emotional refuge."
 
 $('#paragraph1').text(paragraph1)
 
